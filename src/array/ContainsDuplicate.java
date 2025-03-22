c4985f8d6d651269d8a46697dd4d62a6f2c38eb8
gokulmpp <gokulmpp003@gmail.com>
2025-03-22 19:42:08 +0530

    ContainsDuplicate initial

---
+++ src/array/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+package array;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean hasDuplicate(int[] nums) {
+        return false;
+    }
+}
