c3c0dfe7857b504ae07d7c8edbff66f9f7e3ac21
gokulmpp <gokulmpp003@gmail.com>
2025-03-23 20:42:49 +0530

    arrays checking

--- src/array/ContainsDuplicate.java
+++ src/array/ContainsDuplicate.java
@@ -1,11 +1,24 @@
 package array;
 
+import java.util.Arrays;
+
 public class ContainsDuplicate {
-    public static void main(String[] args) {
-
-    }
-
     public boolean hasDuplicate(int[] nums) {
-        return true;
+        Arrays.sort(nums);
+        for(int i=1;i<=nums.length-1;i++)
+        {
+            if(nums[i]==nums[i-1])
+            {
+                return true;
+            }
+        }return false;
     }
+    public static void main(String[] args) {
+        ContainsDuplicate c=new ContainsDuplicate();
+        int[] num={1,2,3,3};
+        System.out.println(c.hasDuplicate(num));
+
+    }
+
+
 }
